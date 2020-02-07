58c765545f9861a813b70943ffbfbaacc0795298
Mykyta Pysarenko <mpysarenko@griddynamics.com>
2020-02-07 18:47:03 +0200

    task n5

--- index.js
+++ index.js
@@ -70,3 +70,40 @@ function nextInLine(arr, item) {
   return arr.shift();
 }
 //Task 5
+let contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"]
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwards", "Magic", "Hagrid"]
+  },
+  {
+    firstName: "Sherlok",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"]
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"]
+  }
+];
+
+function lookUpProfile(firstName, prop) {
+  for (let person of contacts) {
+    if (person.firstName === firstName) {
+      const userInform = person[prop];
+      return userInform ? userInform : "No such property";
+    }
+  }
+  return "No such contact";
+}
+console.log(lookUpProfile("Sherlok", "lastNme"));
