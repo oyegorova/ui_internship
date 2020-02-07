18cba3af1264b97337fd49208b974b122cd60466
Mykyta Pysarenko <mpysarenko@griddynamics.com>
2020-02-07 13:46:25 +0200

    task n1

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-/* initial file */
+function golfScore(par, strokes) {
+  //Task 1
+  if (par < 0 || strokes < 0) {
+    return "Error";
+  } else if (typeof par === "string" || typeof par === "string") {
+    return "Error2";
+  } else if (strokes === 1) {
+    return "Hole-in-one!";
+  } else if (strokes <= par - 2) {
+    return "Eagle";
+  } else if (strokes === par - 1) {
+    return "Birdie";
+  } else if (strokes === par) {
+    return "Par";
+  } else if (strokes === par + 1) {
+    return "Bogey";
+  } else if (strokes === par + 2) {
+    return "Double Bogey";
+  } else if (strokes >= par + 3) {
+    return "Go Home!";
+  }
+}
