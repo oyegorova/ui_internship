f4f67c7a521e5ced73922aad0371b0d94622033f
Mykyta Pysarenko <mpysarenko@griddynamics.com>
2020-02-13 16:44:43 +0200

    tasks26-35

--- index.js
+++ index.js
@@ -165,20 +165,20 @@ const confirmEnding = (str, target) => {
     return true;
   }
   return false;
-}
+};
 
 //Task 14
 
 const cloneArray = arr => {
   let newArr = [...arr];
   return newArr;
-}
+};
 
 //Task 13
 
 const largestOfFour = arr => {
   return arr.map(a => Math.max(...a));
-}
+};
 
 //Task 12
 
@@ -189,4 +189,131 @@ const titleCase = str => {
       return element[0].toUpperCase() + element.substring(1).toLowerCase();
     })
     .join(" ");
+};
+
+//Task 26
+
+const getAverage = marks => {
+  const allValues = marks.reduce((accum, curVal) => accum + curVal);
+  return Math.round(allValues / marks.length);
+};
+
+//Task 27
+
+class Human {
+  constructor() {}
+}
+class Man extends Human {
+  sex = "male";
+  constructor(name) {
+    super();
+    this.name = name;
+  }
+}
+class Woman extends Human {
+  sex = "female";
+  constructor(name) {
+    super();
+    this.name = name;
+  }
+}
+
+class God {
+  constructor() {}
+  static create(manName, womanName) {
+    let man = new Man();
+    man.name = manName;
+    let woman = new Woman();
+    woman.name = womanName;
+    const arr = [];
+    arr.push(man);
+    arr.push(woman);
+    return arr;
+  }
+}
+
+//Task 28
+
+const getVowelCount = str => {
+  let volwesCount = 0;
+  let internArr = str.split("");
+  for (let i = 0; i < internArr.length; i++) {
+    if (
+      internArr[i] === "a" ||
+      internArr[i] === "e" ||
+      internArr[i] === "i" ||
+      internArr[i] === "o" ||
+      internArr[i] === "u"
+    ) {
+      volwesCount++;
+    }
+  }
+  return volwesCount;
+};
+
+//Task 29
+
+const removeDuplicates = arr => {
+  return arr.filter((element, ind) => arr.indexOf(element) == ind);
+};
+
+//Task 30
+
+const peopleInBus = busStops => {
+  const getInt = busStops.reduce((accum, curVal) => {
+    return accum + curVal[1];
+  }, 0);
+  const getOut = busStops.reduce((accum, curVal) => {
+    return accum + curVal[0];
+  }, 0);
+  return getOut - getInt;
+};
+
+//Task 31
+
+const reverseStringInRange = (str, range) => {
+  const arrOfStr = str.split("");
+  const piceOfArr = arrOfStr.slice(range[0], range[1] + 1).reverse();
+  arrOfStr.splice(range[0], piceOfArr.length, ...piceOfArr);
+  return arrOfStr;
+};
+
+//Task 32
+
+const numberToArray = num => {
+  return String(num)
+    .split("")
+    .reverse();
+};
+
+//Task 33
+
+const arrayPlusArray = (arr1, arr2) => {
+  let corArr = arr1.length >= arr2.length ? arr1 : arr2;
+  let anArr = corArr == arr1 ? arr2 : arr1;
+  return corArr.map((el, ind) => {
+    if (anArr[ind] === undefined) {
+      anArr[ind] = 0;
+    }
+    return el + anArr[ind];
+  });
+};
+
+//Task 34
+
+const maxTripletSum = arr => {
+  const sortedArrWithoutDubl = arr
+    .filter((element, ind) => arr.indexOf(element) == ind)
+    .sort((a, b) => b - a);
+  const summ =
+    sortedArrWithoutDubl[0] + sortedArrWithoutDubl[1] + sortedArrWithoutDubl[2];
+  return summ;
+};
+
+//Task 35
+
+const arrayLeaders = arr => {
+  return arr.filter((elem, i) => {
+    return elem > arr.slice(i + 1, arr.length).reduce((a, b) => a + b, 0);
+  });
 };
