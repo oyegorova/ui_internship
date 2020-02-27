9ca476126b70f57338e38e3446325627bbb2698a
Mykyta Pysarenko <mpysarenko@griddynamics.com>
2020-02-27 16:12:11 +0200

    menu unref

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 /* initial file */
+// const correctId = document.querySelector("#menu-icon");
+// const correctClass = document.querySelector(".fa-bars");
+
+$(document).ready(function() {
+  $("#menu-icon").on("click", function() {
+    $(this).toggleClass("fa-bars");
+    $(this).toggleClass("fa-times");
+  });
+});
