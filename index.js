5d3f3613057ec92cebc2c7b4fe367d49b5f9f063
Mykyta Pysarenko <mpysarenko@griddynamics.com>
2020-03-02 12:37:24 +0200

    tasks76-85

--- index.js
+++ index.js
@@ -922,3 +922,181 @@ const DNAStrand = str => {
   const correctStr = incorArr.join("");
   return correctStr;
 };
+
+//Task 76
+
+const deepClone = obj => {
+  let newObj = JSON.parse(JSON.stringify(obj));
+  return newObj;
+};
+
+//Task 77
+
+const expressionMatter = (a, b, c) =>
+  Math.max(a + b + c, a * (b + c), (a + b) * c, a * b * c);
+
+//Task 78
+
+const validBraces = braces => {
+  while (/\(\)|\[\]|\{\}/g.test(braces)) {
+    braces = braces.replace(/\(\)|\[\]|\{\}/g, "");
+  }
+  return !braces.length;
+};
+
+//Task 79
+
+function isPrimeNumber(val) {
+  if (val === 1 || val === 0) return false;
+  let i = 2;
+  const limit = Math.sqrt(val);
+  while (i <= limit) {
+    if (val % i === 0 || val === 1) {
+      return false;
+    }
+    i += 1;
+  }
+  return true;
+}
+
+const numPrimorial = n => {
+  const numArr = [];
+  counter = 1;
+  do {
+    counter++;
+    if (isPrimeNumber(counter)) {
+      numArr.push(counter);
+    }
+  } while (numArr.length < n);
+  return numArr.reduce((a, b) => a * b);
+};
+
+//Task 80
+
+function isPrimeVal(val) {
+  if (val === 1 || val === 0) return false;
+  let i = 2;
+  const limit = Math.sqrt(val);
+  while (i <= limit) {
+    if (val % i === 0) {
+      return false;
+    }
+    i += 1;
+  }
+  return true;
+}
+const sumPrimeIndexedElements = arr => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (isPrimeVal(i)) {
+      sum += arr[i];
+    }
+  }
+  return sum;
+};
+
+//Task 81
+
+const nbMonths = (
+  startPriceOld,
+  startPriceNew,
+  savingsPerMonth,
+  percentLossByMonth
+) => {
+  let months = 0;
+  let moneySaved = 0;
+
+  while (startPriceNew > startPriceOld + moneySaved) {
+    moneySaved += savingsPerMonth;
+    startPriceOld -= startPriceOld * (percentLossByMonth / 100);
+    startPriceNew -= startPriceNew * (percentLossByMonth / 100);
+    months++;
+    if (months % 2 == 1) {
+      percentLossByMonth += 0.5;
+    }
+  }
+  return [months, Math.round(startPriceOld + moneySaved - startPriceNew)];
+};
+
+//Task 82
+
+const bouncingBall = (h, bounce, window) => {
+  if (bounce >= 1 || bounce < 0 || window > h) return -1;
+
+  let count = 0;
+
+  while (h > window) {
+    h = h * bounce;
+    count += 2;
+  }
+  return count - 1;
+};
+
+//Task 83
+
+const findMissing = arr => {
+  const testConstant1 = arr[1] - arr[0];
+  const testConstant2 = arr[2] - arr[1];
+  if (testConstant1 === testConstant2) {
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] - arr[i - 1] != testConstant1) {
+        return arr[i - 1] + testConstant1;
+      }
+    }
+  } else {
+    const corConstant = Math.min(testConstant1, testConstant2);
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] - arr[i - 1] != corConstant) {
+        return arr[i - 1] + corConstant;
+      }
+    }
+  }
+};
+
+//Task 84
+
+const addedChar = (str1, str2) => {
+  const arr1 = str1.split("");
+  const delimitedArr1 = [];
+  let counter1 = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] != arr1[i + 1]) {
+      delimitedArr1.push(arr1.slice(counter1, i + 1).join(""));
+      counter1 = i + 1;
+    }
+  }
+
+  const arr2 = str2.split("");
+  const delimitedArr2 = [];
+  let counter2 = 0;
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i] != arr2[i + 1]) {
+      delimitedArr2.push(arr2.slice(counter2, i + 1).join(""));
+      counter2 = i + 1;
+    }
+  }
+
+  let res = [];
+  delimitedArr2.filter(word => {
+    if (delimitedArr1.indexOf(word) === -1) {
+      res.push(word);
+    }
+  });
+  return res[0][0];
+};
+
+//Task 85
+
+const validate_bet = (arr, str) => {
+  if (str.replace(/[[0-9 ,]/g, "").length > 0) return null;
+  const arrNum = str
+    .replace(/[^[0-9 ,]/g, "")
+    .replace(/,/g, " ")
+    .split(" ")
+    .filter(elem => elem.length != "")
+    .sort();
+  for (let i = 0; i < arrNum.length; i++) {
+    if (arrNum[i] > arr[1] || arrNum.length < arr[0]) return null;
+  }
+  return arrNum;
+};
